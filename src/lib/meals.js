2dbfc77893100cf69fc7a96a5bf1979531d2860e
Shakir Chhawniwala <shakir@themetromaxgroup.com>
2024-07-01 13:44:40 +0530

    sanitizing data for form fileds

--- src/lib/meals.js
+++ src/lib/meals.js
@@ -1,4 +1,7 @@
 import sql from "better-sqlite3"
+import xss from "xss"
+import slugify from "slugify"
+
 const db = sql("meals.db")
 
 export async function getMeals() {
@@ -10,3 +13,8 @@ export function getMeal(slug) {
     //await new Promise((resolve) => setTimeout(resolve, 2000))
     return db.prepare("SELECT * from meals WHERE slug = ?").get(slug)
 } 
+
+export function saveMeal(meal) {
+  meal.slug = slugify(meal.title, { lower: true })
+  meal.instructions = xss(meal.instructions)
+}
